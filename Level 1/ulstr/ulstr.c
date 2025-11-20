cd414d78e10972b89e0becd7329709dc0e2337b1
Stephen Tee <stephentee88@gmail.com>
2025-11-20 20:45:47 +0800

    Update ulstr.c

--- Level 1/ulstr/ulstr.c
+++ Level 1/ulstr/ulstr.c
@@ -5,13 +5,6 @@ void ft_putchar(char c)
     write(1, &c, 1);
 }
 
-int ft_isalpha(char c)
-{
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        return (1);
-    return (0);
-}
-
 int main(int ac, char **av)
 {
     if (ac == 2)
@@ -19,13 +12,10 @@ int main(int ac, char **av)
         int i = 0;
         while (av[1][i] != '\0')
         {
-            if (ft_isalpha(av[1][i]) == 1)
-            {
-                if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-                    ft_putchar(av[1][i] + 32);
-                else
-                    ft_putchar(av[1][i] - 32);
-            }
+            if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+                ft_putchar(av[1][i] + 32);
+            else if (av[1][i] >= 'a' && av[1][i] <= 'z')
+                ft_putchar(av[1][i] - 32);
             else
                 ft_putchar(av[1][i]);
             i++;
